a5a024d2bdcd602b424b61f6b437cbb33fee6225
hermes <heremskd35@gmail.com>
2025-12-07 18:15:48 +0800

    feat: particle trying out

--- chapter2_17/src/script.js
+++ chapter2_17/src/script.js
@@ -18,15 +18,50 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const starTexture = textureLoader.load('./textures/particles/11.png')
 
 /**
- * Test cube
+ *  Particles
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
+
+//Geometry
+// const particlesGeometry = new THREE.SphereGeometry(1, 32, 32);
+const particlesGeometry = new THREE.BufferGeometry();
+//Custom geometry
+const count = 20000;
+
+//Before adding positions/vertices, we need to add the position attribute
+const positions = new Float32Array(count * 3); //3 because each vertex has x,y,z
+const colors = new Float32Array(count * 3)
+
+for(let i =0 ; i < count * 3; i++){
+    positions[i] = (Math.random() - 0.5) * 10;
+    colors[i] = Math.random();
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
+
+
+//Materials
+const particleMaterial = new THREE.PointsMaterial({
+    size: 0.1,
+    withSizeAttenuation: true,
+    transparent: true,
+    alphaMap: starTexture,
+    // alphaTest: 0.001,
+//    depthTest: false,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
+    vertexColors: true
+})
+particleMaterial.map = starTexture;
+
+//Points
+const particles = new THREE.Points(particlesGeometry, particleMaterial);
+
+scene.add(particles);
+
 
 /**
  * Sizes
@@ -80,6 +115,19 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+
+    //Update particles
+    // particles.position.y = Math.sin(elapsedTime * 0.2);
+    
+    for(let i = 0; i < count; i++){
+        const i3 = i * 3;
+
+        const x = particlesGeometry.attributes.position.array[i3];
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x);
+    }
+
+    particlesGeometry.attributes.position.needsUpdate = true;
 
     // Update controls
     controls.update()
